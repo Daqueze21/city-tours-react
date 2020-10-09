dada6d5fd802d5b9b082eaffa123b73685746cac
Dzmitry Bryt <bdv212@yandex.ru>
2020-10-09 18:54:59 +0300

    feat: add function to remove  tour cards

--- src/components/TourList/TourList.js
+++ src/components/TourList/TourList.js
@@ -5,20 +5,28 @@ import {tourData} from '../tourData';
 
 
 class TourList extends Component {
-  state = {
-    tours: tourData
-  };
-  render() {
-   const {tours} = this.state;
-   
-    return (
-      <section className='tourList'>
-        {tours.map(tour => {
-           return <Tour key = {tour.id} tour = {tour} />
-        })}
-      </section>
-    );
-  }
+   state = {
+      tours: tourData
+   };
+
+   removeTour = id => {
+      const {tours} = this.state;
+      const sortedTours = tours.filter(tour  => tour.id !== id);
+      this.setState({
+         tours: sortedTours
+      });
+   };
+   render() {
+      const {tours} = this.state;
+      
+      return (
+         <section className='tourList'>
+         {tours.map(tour => {
+            return <Tour key={tour.id} tour={tour} removeTour = {this.removeTour}/>;
+         })}
+         </section>
+      );
+   }
 }
 
 export default TourList;
